1a595d5317a95e412a0ade266a90f72f391d53be
nadim0835 <nadim0835@gmail.com>
2023-11-21 21:41:39 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    return $(".print").typeText({
+      then: function() {
+        return this.typeText(".......................", {
+          typeSpeed: 250,
+          then: function() {
+            return this.typeText(".......................", {
+              typeSpeed: 500
+            });
+          }
+        });
+      }
+    });
+  });
+  
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
